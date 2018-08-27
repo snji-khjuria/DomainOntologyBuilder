78c0758071d7d8962a95b8996058376ee96e3a24
Sanjeev Kumar <snji.khjuria@gmail.com>
2018-08-27 23:23:37 +0530

    Build complete repository

---
+++ PatternMatcherUtil.py
@@ -0,0 +1,65 @@
+from FileUtil import readPlainHtmlPageContent
+
+# from MatchersUtil import getAllEntitiesInsideLeftRightPattern, getAllRelations, getClusterInsideLeftRightPattern, getElementsOfCluster
+from RegexMatcherUtil import getAllEntitiesInsideLeftRightPattern, getAllRelations, getClusterInsideLeftRightPattern, getElementsOfCluster
+
+def extractProductTitle(pageContent, numPageContent, titlePattern):
+    (leftTitlePattern, rightTitlePattern, patternType) = titlePattern
+    if patternType == "NUM":
+        pageContent = numPageContent
+    return getAllEntitiesInsideLeftRightPattern(pageContent, leftTitlePattern, rightTitlePattern)
+
+
+def extractProductTitles(pageContent, numPageContent, titlePatterns):
+    output = []
+    for titlePattern in titlePatterns:
+        output.extend(extractProductTitle(pageContent, numPageContent, titlePattern))
+    return output
+
+
+def extractProductSpec(pageContent, numPageContent, productSpecsPattern):
+    (leftSpecsPattern, insideSpecsPattern, rightSpecsPattern, patternType) = productSpecsPattern
+    if patternType == "NUM":
+        pageContent = numPageContent
+    found = getClusterInsideLeftRightPattern(pageContent, leftSpecsPattern, insideSpecsPattern, rightSpecsPattern)
+    totalFound = []
+    for cluster in found:
+        clusterItems = getElementsOfCluster(cluster, insideSpecsPattern)
+        totalFound.extend(clusterItems)
+    totalFound = set(totalFound)
+    return list(totalFound)
+    # return getClusterInsideLeftRightPattern(pageContent, leftSpecsPattern, insideSpecsPattern, rightSpecsPattern)
+
+def extractProductSpecs(pageContent, numPageContent, productSpecsPatterns):
+    output = []
+    for pattern in productSpecsPatterns:
+        output.extend(extractProductSpec(pageContent, numPageContent, pattern))
+    return output
+
+
+def extractProductRelation(pageContent, numPageContent, productRelationPattern):
+    (leftTablePattern, middleTablePattern, rightTablePattern, patternType) = productRelationPattern
+    if patternType=="NUM":
+        pageContent = numPageContent
+    # print("Going to find the pattern results")
+    found = getAllRelations(leftTablePattern, middleTablePattern, rightTablePattern, pageContent)
+    return found
+
+def extractProductRelations(pageContent, numPageContent, productRelationPatterns):
+    output = []
+    for pattern in productRelationPatterns:
+        output.extend(extractProductRelation(pageContent, numPageContent, pattern))
+    return output
+
+from StringUtil import replaceNumWordsInStr
+from ProductInfo import ProductInfo
+def getProdInfo(patterns, pageLocation):
+    pageContent        = readPlainHtmlPageContent(pageLocation)
+    numProcessedPageContent = replaceNumWordsInStr(pageContent)
+    titlePatterns      = patterns.getProductTitlePatterns()
+    specsPatterns      = patterns.getProductSpecsPatterns()
+    relationPatterns   = patterns.getProductRelationPatterns()
+    productTitles      = extractProductTitles(pageContent, numProcessedPageContent, titlePatterns)
+    productSpecs       = extractProductSpecs(pageContent, numProcessedPageContent, specsPatterns)
+    productRelations   = extractProductRelations(pageContent, numProcessedPageContent, relationPatterns)
+    return ProductInfo(pageLocation, productTitles, productSpecs, productRelations)
---
+++ StringUtil.py
@@ -0,0 +1,31 @@
+import re
+def replaceMultiWhitespaceWithSingle(str):
+    return re.sub('\s+', ' ', str).strip()
+
+
+def replaceHtmlTags(str):
+    notspace =  re.sub('&nbsp;', ' ', str).strip()
+    notAmp   = re.sub('&amp;', '&', notspace).strip()
+    return notAmp
+
+def replaceNumWordsInStr(s):
+    return re.sub("(<[\s\S]*?)[ \"\'=:]\d+[ \"\']([\s\S]*?>)", r"\1 NUM \2", s)
+
+
+# def replaceNumWordsInStr(s):
+#     return re.sub("(<[\s\S]*)[ \"\'=:]\d+[ \"\']([\s\S]*>)", r"\1 NUM \2", s)
+    # return re.sub("[ \"\'=:]\d+[ \"\']", " NUM ", s)
+
+def processNumInContext(corpusLevelContext):
+    output = []
+    for seedLevelContext in corpusLevelContext:
+        seedLevelOutput = []
+        for eachPattern in seedLevelContext:
+            patternAsList = list(eachPattern)
+            pOutputAsList = []
+            for item in patternAsList:
+                item = replaceNumWordsInStr(item)
+                pOutputAsList.append(item)
+            seedLevelOutput.append(tuple(pOutputAsList))
+        output.append(seedLevelOutput)
+    return output
